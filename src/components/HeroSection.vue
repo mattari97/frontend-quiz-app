372130c9ffee4561f786e0c77a734a82b34a3d39
Mattari <ac.dev@hotmail.com>
2025-03-02 16:09:01 +0100

    Extract hero section component

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+  titleBold: string;
+  caption: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <section class="hero-section">
+    <h1 class="hero-section__title">
+      <span class="hero-section__title-normal">{{ props.title }}</span>
+      <span class="hero-section__title-bold">{{ props.titleBold }}</span>
+    </h1>
+    <p class="hero-section__caption">{{ props.caption }}</p>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.hero-section {
+  display: grid;
+  row-gap: 1rem;
+  padding: 2rem 1.5rem 2.5rem;
+
+  &__title {
+    display: grid;
+    row-gap: 0.5rem;
+    color: hsl(var(--clr-heading));
+    font-size: var(--fs-heading-lg);
+    line-height: var(--lh-none);
+
+    &-normal {
+      display: block;
+    }
+
+    &-bold {
+      font-weight: var(--fw-medium);
+    }
+  }
+
+  &__caption {
+    font-style: italic;
+  }
+}
+</style>
--- src/views/WelcomeVue.vue
+++ src/views/WelcomeVue.vue
@@ -2,17 +2,12 @@
 import { quizzes } from '@/assets/json/quizzes.json';
 import welcomeData from '@/assets/json/welcome.json';
 import CategoryCard from '@/components/CategoryCard.vue';
+import HeroSection from '@/components/HeroSection.vue';
 </script>
 
 <template>
   <main class="welcome">
-    <section class="welcome-hero">
-      <h1 class="welcome-hero__title">
-        <span>{{ welcomeData.title }}</span>
-        <span>{{ welcomeData.titleBold }}</span>
-      </h1>
-      <p class="welcome-hero__caption">{{ welcomeData.caption }}</p>
-    </section>
+    <HeroSection v-bind="welcomeData" />
     <section class="welcome-categories">
       <CategoryCard
         v-for="(quiz, index) in quizzes"
@@ -29,32 +24,6 @@ import CategoryCard from '@/components/CategoryCard.vue';
 .welcome {
   display: grid;
   grid-template-rows: auto 1fr;
-}
-
-.welcome-hero {
-  display: grid;
-  row-gap: 1rem;
-  padding: 2rem 1.5rem 2.5rem;
-
-  &__title {
-    display: grid;
-    row-gap: 0.5rem;
-    color: hsl(var(--clr-heading));
-    font-size: var(--fs-heading-lg);
-    line-height: var(--lh-none);
-
-    & > span {
-      display: block;
-    }
-
-    & > span:last-of-type {
-      font-weight: var(--fw-medium);
-    }
-  }
-
-  &__caption {
-    font-style: italic;
-  }
 }
 
 .welcome-categories {
